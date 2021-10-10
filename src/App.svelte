f9b3060c55b6296a61632d5279020f33d9547686
Jasper Meggitt <jasper.meggitt@gmail.com>
2021-10-10 05:20:02 -0400

    Apply Semantic UI to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,32 @@
 <script>
-    import 'codemirror/mode/javascript/javascript'
-    import CodeMirror from '@joshnuss/svelte-codemirror'
+    import "semantic-ui-css/semantic.css";
+    import 'codemirror/mode/javascript/javascript';
+    import CodeMirror from '@joshnuss/svelte-codemirror';
+
+    const config = {
+        lineNumbers: true,
+        mode: "javascript"
+    };
 
     let editor;
     let code = "const x = 42";
     let filesPromise = ["loading..."];
-    let saveName = "";
+    let saveName = "untitled.js";
+
+    let currentFile = "unnamed.js";
 
     // Request file list be loaded upon page opened
     getFiles();
+
+    function parentResize(parent) {
+        console.log("Resize!");
+        // const parent = event.target;
+        const totalHeight = parent.height;
+        const offset = editor.offsetTop;
+        console.log("Total height:" + parent);
+        console.log("Offset:" + offset);
+        editor.setSize(editor.width, totalHeight - offset);
+    }
 
     function getFiles() {
         fetch('/files', {
@@ -19,12 +37,13 @@
     }
 
     function loadFile(event) {
-        const name = event.target.getAttribute("name");
+        currentFile = event.target.getAttribute("name");
+        saveName = currentFile;
 
         fetch("/load", {
             method: "POST",
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({name})
+            body: JSON.stringify({name: currentFile})
         })
             .then(response => response.json())
             .then(json => {
@@ -42,7 +61,7 @@
             code: textToWrite
         };
 
-        console.log("Saving file")
+        console.log("Saving file");
 
         fetch("/save", {
             method: "POST",
@@ -59,44 +78,35 @@
     }
 </script>
 
-<div class="box">
-    <div class="sidebar">
+<div class="ui bottom attached segment pushable">
+    <div class="ui visible inverted left vertical sidebar menu">
         {#await filesPromise then files}
-            <ul>
-                {#each files as f}
-                    <li name={f} on:click={loadFile}>{f}.js</li>
-                {/each}
-            </ul>
+            {#each files as file}
+                <a name={file} on:click={loadFile} class="{file === currentFile ? 'active' : ''} item">
+                    {file}
+                </a>
+            {/each}
         {/await}
     </div>
-
-    <div class="codebox">
-        <CodeMirror bind:editor options={{ lineNumbers: true, mode: "javascript"}} class="editor" value={code}/>
+    <div class="pusher">
+        <div class="ui basic bottom attached segment">
+            <h3 class="ui header">
+                <div class="ui transparent icon input">
+                    <a on:click={saveFile}>
+                        <i class="save icon"></i>
+                    </a>
+                    <input type="text" placeholder="untitled.js" bind:value={saveName}/>
+                </div>
+            </h3>
+            <div class="codeArea">
+                <CodeMirror bind:editor options={config} value={code}/>
+            </div>
+        </div>
     </div>
 </div>
 
-<form action="">
-    <input type="text" bind:value={saveName}/>
-</form>
-<button on:click={saveFile}>
-    Save
-</button>
-
 <style>
-
-    .box {
-        display: flex;
-        flex-direction: row;
-        justify-content: flex-start;
-        flex-wrap: nowrap;
-    }
-
-    :global(.editor) {
-        /* Set height, width, borders, and global font properties here */
-        font-family: monospace;
-        height: 300px;
-        direction: ltr;
-        color: var(--cm-text-color);
-        background: var(--cm-background-color);
+    CodeMirror {
+        /*  *Demonic Screeching*  */
     }
 </style>
